89d0341c487603c0c5c330b98533b6b1b2ff301b
Peter Rice <peter@peterrice.xyz>
2021-04-11 16:34:35 -0400

    qpm alias

--- home-manager/fish.nix
+++ home-manager/fish.nix
@@ -85,6 +85,7 @@
       tree = "${pkgs.exa}/bin/exa -T";
       bell = "echo \\a";
       mp = "mpc toggle";
+      qpm = "qbpm";
       "hoi4" = "steam steam://rungameid/394360";
       "eu4" = "steam steam://rungameid/236850";
       "ck3" = "steam steam://rungameid/1158310";
