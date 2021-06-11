950531a4c934ff2da480678e86058fa0ba136b72
Peter Rice <peter@peterrice.xyz>
2021-06-11 17:15:49 -0400

    replace the old crossbell public key

--- users/peter.nix
+++ users/peter.nix
@@ -5,8 +5,8 @@
     extraGroups = [ "wheel" "transmission" ];
     openssh.authorizedKeys.keys = [
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILACfyJt7+ULfX1XFhBbztlTMNDZnRNQbKj5DV2S7uVo peter@grancel"
-      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKQzVZRQGL+282Zk6pPBO2tni2iCRpCi8cNUMcNMpiuX peter@vps221009.vps.ovh.ca"
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJ9oTGdaddqjAM93FQP83XABhVxZo1jo8ljb62CtUoBq peter@ruan"
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMgVQjYQ3EKuiXE3b7wn8vADTTMO/Jm7Vf+PRzgkTkG/ peter@vps221009"
       "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDR/4VIeWmdlbrR0lHNjwpZmr4+jqJ+CslwJfe+BjbKTOjelAiOnUjwQHZFyhQwwc56ZHfw438AHxwOzMBPZSMHQDtJG3F1tXjqErqjFjSz7axyFkxXcmdlTJKkTmRuM9KmkyNBdi9JAqoXnWJFH7kMxNM0WB125SSM0T4arvKd2BdAFVyE+LgAobbtXELQZFfncH88Jlbm1qyzYeiMXHRUJDd3z+0JmZrg5/3NniGWoVSkm+fVyuragxwAR+bHU0XEI6LwmljfNw91PqTDtRmhDN0rN93TjknonqxHTIftZbEnz2aK46s57eHAY7Wmy4dyy2ZSht2mChLDyRTHQWvF Created for price@hubspot.com by Kickstart"
     ];
   };
