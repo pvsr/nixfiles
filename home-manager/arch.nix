9a27c5a0285d61701ce8d2914440ddf8b13d3b6a
Peter Rice <peter@peterrice.xyz>
2021-08-14 16:51:51 -0400

    enable beets

--- home-manager/arch.nix
+++ home-manager/arch.nix
@@ -17,25 +17,5 @@
     transmission
   ];
 
-  programs.beets.enable = false;
-  programs.beets.settings = {
-    directory = "~/music";
-    library = "~/library.blb";
-    plugins = [ "random" "scrub" "ftintitle" "embedart" "fetchart" "chroma" "lyrics" "scrub" ];
-    original_date = true;
-    from_scratch = true;
-    mpd.host = "localhost";
-    mpd.port = 6600;
-    match.preferred = {
-      countries = [ "XW" "US" "JP" ];
-      media = [ "CD" "Digital Media|File" ];
-      original_year = true;
-    };
-    ftintitle.drop = true;
-    embedart.remove_art_file = true;
-    fetchart.sources = [ "coverart" "filesystem" ];
-    scrub.auto = true;
-  };
-
   #home.stateVersion = "21.05";
 }
--- home-manager/common.nix
+++ home-manager/common.nix
@@ -125,4 +125,25 @@ let colors = import ./colors.nix; in
       };
     };
   };
+
+  programs.beets.enable = true;
+  programs.beets.settings = {
+    directory = "~/music";
+    library = "~/library.blb";
+    plugins = [ "random" "scrub" "ftintitle" "embedart" "fetchart" "chroma" "lyrics" "scrub" ];
+    original_date = true;
+    from_scratch = true;
+    mpd.host = "localhost";
+    mpd.port = 6600;
+    match.preferred = {
+      countries = [ "XW" "US" "JP" ];
+      media = [ "CD" "Digital Media|File" ];
+      original_year = true;
+    };
+    ftintitle.drop = true;
+    embedart.remove_art_file = true;
+    fetchart.sources = [ "coverart" "filesystem" ];
+    scrub.auto = true;
+  };
+
 }
