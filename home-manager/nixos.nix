f028f93d248a2b3370c50113776585c52a8d4cea
Peter Rice <peter@peterrice.xyz>
2021-09-06 15:35:23 -0400

    beets

--- home-manager/nixos.nix
+++ home-manager/nixos.nix
@@ -16,19 +16,16 @@
   programs.beets.settings = {
     directory = "~/music";
     library = "~/library.blb";
-    plugins = [ "random" "scrub" "ftintitle" "embedart" "fetchart" "chroma" "lyrics" "scrub" ];
+    plugins = [ "random" "scrub" "ftintitle" "fetchart" "chroma" ];
     original_date = true;
     from_scratch = true;
     mpd.host = "localhost";
     mpd.port = 6600;
     match.preferred = {
       countries = [ "XW" "US" "JP" ];
-      media = [ "CD" "Digital Media|File" ];
+      media = [ "Digital Media|File" "CD" ];
       original_year = true;
     };
-    ftintitle.drop = true;
-    embedart.remove_art_file = true;
-    fetchart.sources = [ "coverart" "filesystem" ];
     scrub.auto = true;
   };
 }
