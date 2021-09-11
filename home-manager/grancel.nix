fc432fa060e9ee032e01dd5aa7839ed8260d6491
Peter Rice <peter@peterrice.xyz>
2021-09-11 16:49:50 -0400

    absolute path to pass

--- home-manager/grancel.nix
+++ home-manager/grancel.nix
@@ -17,8 +17,8 @@
     enable = true;
     package = pkgs.spotifyd.override { withMpris = true; };
     settings.global = {
-      username_cmd = "pass show spotify/userid";
-      password_cmd = "pass show spotify.com";
+      username_cmd = "${pkgs.pass}/bin/pass show spotify/userid";
+      password_cmd = "${pkgs.pass}/bin/pass show spotify.com";
       backend = "pulseaudio";
     };
   };
