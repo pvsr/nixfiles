c256f1ec252c97505a855fb6c664a4fe7a784afb
Peter Rice <peter@peterrice.xyz>
2021-09-11 12:18:45 -0400

    fewer nixpkgs instances

--- flake.nix
+++ flake.nix
@@ -11,7 +11,9 @@
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
 
     agenix.url = github:ryantm/agenix;
+    agenix.inputs.nixpkgs.follows = "nixpkgs";
     deploy-rs.url = github:serokell/deploy-rs;
+    deploy-rs.inputs.nixpkgs.follows = "nixpkgs";
     qbpm.url = github:pvsr/qbpm;
     qbpm.inputs.nixpkgs.follows = "nixpkgs";
 
