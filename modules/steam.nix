7e8e03866a5260cef3c9d994f21f9298986f3696
Peter Rice <peter@peterrice.xyz>
2021-10-10 14:55:47 -0400

    work around nixos steam bug

--- modules/steam.nix
+++ modules/steam.nix
@@ -1,3 +1,9 @@
 { pkgs, ... }: {
   programs.steam.enable = true;
+
+  environment.systemPackages = [
+    (pkgs.steam.override {
+      extraPkgs = pkgs: with pkgs; [ pango harfbuzz libthai ];
+    })
+  ];
 }
