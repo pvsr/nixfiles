55c68ccd7220c8559fdc558d6f460915233ed3ea
Peter Rice <peter@peterrice.xyz>
2022-02-26 00:44:35 -0500

    remove workaround

--- modules/steam.nix
+++ modules/steam.nix
@@ -1,9 +1,3 @@
 { pkgs, ... }: {
   programs.steam.enable = true;
-
-  environment.systemPackages = [
-    (pkgs.steam.override {
-      extraPkgs = pkgs: with pkgs; [ pango harfbuzz libthai ];
-    })
-  ];
 }
