bbcd9ddff6f3c05266b9e784fdb23fb49217adb8
Peter Rice <peter@peterrice.xyz>
2022-03-10 18:16:40 -0500

    lower snapshot retention time

--- hosts/grancel.nix
+++ hosts/grancel.nix
@@ -34,7 +34,7 @@
         onCalendar = "daily";
         settings = {
           snapshot_preserve_min = "2d";
-          snapshot_preserve = "14d";
+          snapshot_preserve = "7d";
           volume = {
             "/media/nixos" = {
               snapshot_dir = "btrbk_snapshots";
