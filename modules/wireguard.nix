e3abe973cf3c27df418b6a350618e8d3d89b8cbf
Peter Rice <peter@peterrice.xyz>
2022-06-05 13:02:36 -0400

    reupgrade to 22.05
    This reverts commit 53e7f9e7a6c5cbacc51d2050c89f11f57aaf45cf.

--- modules/wireguard.nix
+++ modules/wireguard.nix
@@ -38,7 +38,7 @@ let
   peers = map selectEndpoint (lib.attrValues otherHosts);
 in
 {
-  environment.systemPackages = with pkgs; [ wireguard wireguard-tools ];
+  environment.systemPackages = with pkgs; [ wireguard-tools ];
 
   networking.firewall.allowedUDPPorts = [ host.listenPort ];
 
