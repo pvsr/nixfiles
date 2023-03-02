29ee5aa22e2577c06ed24d80072d7bc2540c059a
Peter Rice <peter@peterrice.xyz>
2023-03-01 22:09:55 -0500

    update cache/substituters

--- modules/cachix.nix
+++ modules/cachix.nix
@@ -10,12 +10,12 @@
     nix = {
       extraOptions = "gc-keep-outputs = true";
 
-      binaryCaches = [
+      settings.substituters = [
         "https://cachix.cachix.org"
         "https://nix-community.cachix.org"
         "https://nixpkgs-wayland.cachix.org"
       ];
-      binaryCachePublicKeys = [
+      settings.trusted-public-keys = [
         "cachix.cachix.org-1:eWNHQldwUO7G2VkjpnjDbWwy4KQ/HNxht7H4SSoMckM="
         "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
         "nixpkgs-wayland.cachix.org-1:3lwxaILxMRkVhehr5StQprHdEo4IrE8sRho9R9HOLYA="
