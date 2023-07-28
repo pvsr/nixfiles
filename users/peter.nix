638331863f61123cfb82c62c2b6a9764f1ebf5f4
Peter Rice <peter@peterrice.xyz>
2023-07-28 16:20:02 -0400

    organize keys

--- users/peter.nix
+++ users/peter.nix
@@ -4,13 +4,11 @@
     isNormalUser = true;
     extraGroups = ["wheel" "transmission"];
     openssh.authorizedKeys.keys = [
-      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILACfyJt7+ULfX1XFhBbztlTMNDZnRNQbKj5DV2S7uVo"
-      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJ9oTGdaddqjAM93FQP83XABhVxZo1jo8ljb62CtUoBq"
-      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMgVQjYQ3EKuiXE3b7wn8vADTTMO/Jm7Vf+PRzgkTkG/"
-      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHBMswv0obMNyU9xAYGAxOD/Z0jYemfEONN0u+xeYmUM"
-      "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDR/4VIeWmdlbrR0lHNjwpZmr4+jqJ+CslwJfe+BjbKTOjelAiOnUjwQHZFyhQwwc56ZHfw438AHxwOzMBPZSMHQDtJG3F1tXjqErqjFjSz7axyFkxXcmdlTJKkTmRuM9KmkyNBdi9JAqoXnWJFH7kMxNM0WB125SSM0T4arvKd2BdAFVyE+LgAobbtXELQZFfncH88Jlbm1qyzYeiMXHRUJDd3z+0JmZrg5/3NniGWoVSkm+fVyuragxwAR+bHU0XEI6LwmljfNw91PqTDtRmhDN0rN93TjknonqxHTIftZbEnz2aK46s57eHAY7Wmy4dyy2ZSht2mChLDyRTHQWvF"
-      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKzIvu2+Spcxq92BYAAbfn4wHhgf/p+3P3ntaaLsR9UH"
-      "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQCr1V5CgzNa4A9TGcth3Bk9e+lxckGvqupIe5obvyelJ9PjM+L0zyo7sJmaQnq/SCab2+I01hHl+JfFGXjHmv144WjUvxyOYjwXFZvypzVyP7puDb1+MGa5tdfOAGhhm5WdnE4PJZmJBFSS2b88b2pNMlIXIdbG4LBgb6mgsHS3ztgjU4/OcqbmaZXaJnepiu+E+ihfg2ANkbfrqWxgaWQBXMvb+DMcZTSLvdj+pnTH5qtyGmyRqrJ4wAT3tZCy72aS1VcneGXuvrtswcRBgt9ZFHYTYFHQpoWqXdg/6mM9a4o5CnqD4YsyRvlhzRKKfebRlhqXfrs6B88kpFMeL4xbNF+Grgn21x3kuqNrO/f1PTBzoX3TmhoWoM1oHB4hOoODMBm39j/CThHxQYnDAZfLGh8sN0viJmpxLeXbjW6PX59edfyOoLPzeoeHJD5oAo9oZRTLnD5YcwSAi9aI3XOZ4wANdVRnDOmLLRHmiTmR7t3BNAA6x077cwLhwOMXWuc="
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILACfyJt7+ULfX1XFhBbztlTMNDZnRNQbKj5DV2S7uVo peter@grancel"
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJ9oTGdaddqjAM93FQP83XABhVxZo1jo8ljb62CtUoBq peter@ruan"
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKzIvu2+Spcxq92BYAAbfn4wHhgf/p+3P3ntaaLsR9UH peter@crossbell"
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIAF7gP6ygxWay6wmQlVg5nnQaK1TyabPjYPCeja5VZGv peter@arseille"
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJojCQs1VjUFaO/2dOq2N/zQgfRtBtFE7nLu3VpJZkwt price@jurai"
     ];
   };
 }
