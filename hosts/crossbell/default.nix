40999a50b25247777bc23a31d415653308d5455a
Peter Rice <peter@peterrice.xyz>
2023-08-05 16:52:42 -0400

    headscale dns config

--- hosts/crossbell/default.nix
+++ hosts/crossbell/default.nix
@@ -12,11 +12,18 @@
   time.timeZone = "America/New_York";
 
   users.users.root.openssh.authorizedKeys.keys = ["ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHwIv6+ZEHCVNmIS1vfUO+bqIP2y3hv3h/AzzmvTQ3HI"];
+  environment.systemPackages = [config.services.headscale.package];
   services = {
     headscale.enable = true;
-    headscale.serverUrl = "https://tailscale.peterrice.xyz";
-    headscale.port = 9753;
     headscale.address = "127.0.0.1";
+    headscale.port = 9753;
+    headscale.settings = {
+      ip_prefixes = ["100.64.0.0/10"];
+      server_url = "https://tailscale.peterrice.xyz";
+      dns_config.base_domain = "ts.peterrice.xyz";
+      dns_config.nameservers = ["1.1.1.1"];
+      dns_config.domains = ["peter.ts.peterrice.xyz" "ts.peterrice.xyz"];
+    };
 
     openssh.enable = true;
     openssh.ports = [18325];
