3a1ec3fef353803d2c5f32fc4871bb554121640e
Peter Rice <peter@peterrice.xyz>
2023-09-06 19:52:50 -0400

    jurai: only set xdg user dirs on linux

--- home-manager/common.nix
+++ home-manager/common.nix
@@ -35,18 +35,6 @@ in {
 
   home.language.base = "en-US.UTF-8";
 
-  xdg.userDirs = {
-    enable = true;
-    desktop = "${config.home.homeDirectory}/desktop";
-    documents = "${config.home.homeDirectory}/documents";
-    download = "${config.home.homeDirectory}/downloads";
-    music = "${config.home.homeDirectory}/music";
-    pictures = "${config.home.homeDirectory}/pictures";
-    publicShare = "${config.home.homeDirectory}/public";
-    templates = "${config.home.homeDirectory}/templates";
-    videos = "${config.home.homeDirectory}/videos";
-  };
-
   home.shellAliases = {
     bell = "echo \\a";
 
--- home-manager/nixos.nix
+++ home-manager/nixos.nix
@@ -20,6 +20,18 @@
     gtk.enable = true;
   };
 
+  xdg.userDirs = {
+    enable = true;
+    desktop = "${config.home.homeDirectory}/desktop";
+    documents = "${config.home.homeDirectory}/documents";
+    download = "${config.home.homeDirectory}/downloads";
+    music = "${config.home.homeDirectory}/music";
+    pictures = "${config.home.homeDirectory}/pictures";
+    publicShare = "${config.home.homeDirectory}/public";
+    templates = "${config.home.homeDirectory}/templates";
+    videos = "${config.home.homeDirectory}/videos";
+  };
+
   services.gpg-agent = {
     enable = true;
     defaultCacheTtl = 3 * 60 * 60;
