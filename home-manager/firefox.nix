a444cb3fa84fbbc2d8a6c48bc9e97de2b9cf81c4
Peter Rice <peter@peterrice.xyz>
2024-05-23 00:47:38 -0400

    firefox: update search engingees

--- home-manager/firefox.nix
+++ home-manager/firefox.nix
@@ -12,13 +12,14 @@
         force = true;
         default = "Kagi";
         engines = let
-          daily = 24 * 60 * 60 * 1000;
+          weekly = 7 * 24 * 60 * 60 * 1000;
         in {
           "Bing".metaData.hidden = true;
-          "Amazon.com".metaData.hidden = true;
           "eBay".metaData.hidden = true;
+          "Amazon.com".metaData.hidden = true;
+          "Google".metaData.hidden = true;
+          "DuckDuckGo".metaData.hidden = true;
           "Wikipedia (en)".metaData.alias = "w";
-          "Google".metaData.alias = "g";
           "Kagi" = {
             urls = [
               {
@@ -30,43 +31,43 @@
               }
             ];
             iconUpdateURL = "https://github.com/kagisearch/browser_extensions/blob/e65c723370a2ee3960120612d2d46f3c9bfb6d87/shared/icons/icon_32px.png?raw=true";
-            updateInterval = daily;
+            updateInterval = weekly;
             definedAliases = ["k"];
           };
           "YouTube" = {
             urls = [{template = "https://www.youtube.com/results?search_query={searchTerms}";}];
             iconUpdateURL = "https://www.youtube.com/s/desktop/271dfaff/img/favicon_144x144.png";
-            updateInterval = daily;
+            updateInterval = weekly;
             definedAliases = ["y"];
           };
           "GitHub" = {
             urls = [{template = "https://github.com/search?type=repositories&q={searchTerms}";}];
             iconUpdateURL = "https://github.githubassets.com/favicons/favicon.svg";
-            updateInterval = daily;
+            updateInterval = weekly;
             definedAliases = ["gi"];
           };
           "Wiktionary" = {
             urls = [{template = "https://www.wiktionary.org/search-redirect.php?family=wiktionary&language=en&go=Go&search={searchTerms}";}];
             iconUpdateURL = "https://en.wiktionary.org/static/favicon/wiktionary/en.ico";
-            updateInterval = daily;
+            updateInterval = weekly;
             definedAliases = ["wikt"];
           };
           "Jisho" = {
             urls = [{template = "https://jisho.org/search/{searchTerms}";}];
             iconUpdateURL = "https://assets.jisho.org/assets/favicon-062c4a0240e1e6d72c38aa524742c2d558ee6234497d91dd6b75a182ea823d65.ico";
-            updateInterval = daily;
+            updateInterval = weekly;
             definedAliases = ["ji"];
           };
           "RateYourMusic" = {
             urls = [{template = "https://rateyourmusic.com/search?searchtype=a&searchterm={searchTerms}";}];
             iconUpdateURL = "https://e.snmc.io/3.0/img/logo/sonemic-32.png";
-            updateInterval = daily;
+            updateInterval = weekly;
             definedAliases = ["ra"];
           };
           "Genius" = {
             urls = [{template = "https://genius.com/search?q={searchTerms}";}];
             iconUpdateURL = "https://genius.com/favicon.ico";
-            updateInterval = daily;
+            updateInterval = weekly;
             definedAliases = ["@gen"];
           };
           "Nix Packages" = {
@@ -74,23 +75,22 @@
               {template = "https://search.nixos.org/packages?channel=unstable&query={searchTerms}";}
             ];
             icon = "${pkgs.nixos-icons}/share/icons/hicolor/scalable/apps/nix-snowflake.svg";
-            definedAliases = ["@nxp"];
+            definedAliases = ["@np"];
           };
           "NixOS Options" = {
             urls = [{template = "https://search.nixos.org/options?channel=unstable&query={searchTerms}";}];
             icon = "${pkgs.nixos-icons}/share/icons/hicolor/scalable/apps/nix-snowflake.svg";
-            definedAliases = ["@nxo"];
+            definedAliases = ["@no"];
           };
           "NixOS Wiki" = {
-            urls = [{template = "https://nixos.wiki/index.php?search={searchTerms}";}];
-            iconUpdateURL = "https://nixos.wiki/favicon.png";
-            updateInterval = daily;
-            definedAliases = ["@nxw"];
+            urls = [{template = "https://wiki.nixos.org/index.php?search={searchTerms}";}];
+            icon = "${pkgs.nixos-icons}/share/icons/hicolor/scalable/apps/nix-snowflake.svg";
+            definedAliases = ["@nw"];
           };
           "Python Documentation" = {
             urls = [{template = "https://docs.python.org/3/search.html?check_keywords=yes&area=default&q={searchTerms}";}];
             iconUpdateURL = "https://docs.python.org/3/_static/py.svg";
-            updateInterval = daily;
+            updateInterval = weekly;
             definedAliases = ["pd"];
           };
         };
