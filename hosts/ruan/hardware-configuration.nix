b587bd4d61f227c540a21f79f8e5dbe058306826
Peter Rice <peter@peterrice.xyz>
2024-06-09 01:29:20 -0400

    amd cpus: enable zenpower

--- hosts/ruan/hardware-configuration.nix
+++ hosts/ruan/hardware-configuration.nix
@@ -7,7 +7,7 @@
 }: {
   imports = [
     inputs.nixos-hardware.nixosModules.common-pc-ssd
-    inputs.nixos-hardware.nixosModules.common-cpu-amd
+    inputs.nixos-hardware.nixosModules.common-cpu-amd-zenpower
     inputs.nixos-hardware.nixosModules.common-gpu-amd
   ];
 
