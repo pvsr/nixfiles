30d53d95965f5d641aab4c07c235bdaa05c90da4
Peter Rice <peterrice@fastmail.com>
2024-08-19 03:07:57 -0400

    jj: simpler abbr

--- home-manager/jujutsu.nix
+++ home-manager/jujutsu.nix
@@ -12,23 +12,6 @@
         ui.diff.format = "git";
       };
     };
-    fish = {
-      shellAbbrs = lib.mapAttrs (abbr: command: "jj ${command}") {
-        jn = "new";
-        jds = "describe";
-        je = "edit";
-        jc = "commit";
-        jsp = "split";
-        jsq = "squash";
-        jr = "rebase";
-        jl = "log";
-        jb = "branch";
-        jbs = "branch set";
-        jd = "diff";
-        jst = "status";
-        jg = "git";
-        jp = "git push";
-      };
-    };
+    fish.shellAbbrs.j = "jj";
   };
 }
