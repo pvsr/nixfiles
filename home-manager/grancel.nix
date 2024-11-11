3ab9c66f57dc0647600ee7f4cf92844540c613fa
Peter Rice <peterrice@fastmail.com>
2024-11-11 13:56:48 -0500

    spotifyd: switch to manual auth

--- home-manager/grancel.nix
+++ home-manager/grancel.nix
@@ -20,8 +20,6 @@
     enable = true;
     package = pkgs.spotifyd.override { withMpris = true; };
     settings.global = {
-      username_cmd = "${pkgs.pass}/bin/pass show spotify/userid";
-      password_cmd = "${pkgs.pass}/bin/pass show spotify.com";
       backend = "pulseaudio";
       bitrate = 160;
       initial_volume = "100";
