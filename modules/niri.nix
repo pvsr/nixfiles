42bbbd4471285e4e6e8fb50b0827c3e0d32f4aca
Peter Rice <peterrice@fastmail.com>
2025-01-06 02:38:17 -0500

    niri: use mate polkit agent

--- modules/niri.nix
+++ modules/niri.nix
@@ -9,4 +9,5 @@
 
   programs.niri.enable = true;
   programs.niri.package = pkgs.niri-unstable;
+  systemd.user.services.niri-flake-polkit.serviceConfig.ExecStart = lib.mkForce "${pkgs.mate.mate-polkit}/libexec/polkit-mate-authentication-agent-1";
 }
