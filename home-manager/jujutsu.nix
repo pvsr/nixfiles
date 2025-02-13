273412a8d8c5d6a98e023c49b241b56949371e64
Peter Rice <peterrice@fastmail.com>
2025-02-12 23:01:12 -0500

    jj: move summarize to jj module

--- home-manager/jujutsu.nix
+++ home-manager/jujutsu.nix
@@ -24,6 +24,39 @@
         '';
       };
     };
-    fish.shellAbbrs.j = "jj";
+    fish = {
+      shellAbbrs.j = "jj";
+      functions = {
+        summarize = {
+          wraps = "jj show";
+          body = ''
+            set recent (jj log --no-graph \
+              -r 'ancestors(immutable_heads(), 8) & ~description("flake.lock")' \
+              -T 'separate("\n", separate(" ", \
+                format_timestamp(commit_timestamp(self)), \
+                format_short_commit_id(self.commit_id()) \
+              ), self.description())' \
+            | string collect)
+            PAGER= jj show $argv[1]
+            jj show $argv[1] | llm -s "Write a one-line commit message for \
+            these changes. It should be under 50 characters. If a terse \
+            description does not fit in 50 characters you may continue to write \
+            one or more paragraphs describing the changes, which should be \
+            wrapped at 80 characters. It should match the style of these recent \
+            commit messages in the same repository:
+            $recent
+
+            Include the commit message and nothing else, fenced in triple \
+            backticks like so:
+            ```
+            commit message, less than 50 characters
+
+            optional commit description
+            ```
+            "
+          '';
+        };
+      };
+    };
   };
 }
