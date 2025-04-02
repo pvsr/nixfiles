21d5985a09fb8dc085a5a491baea758448a9f8d8
Peter Rice <peterrice@fastmail.com>
2025-04-02 02:05:28 -0400

    reduce indentation in disko config

--- hosts/grancel/disks.nix
+++ hosts/grancel/disks.nix
@@ -11,57 +11,55 @@
     ];
   };
 
-  disko.devices = {
-    disk = {
-      grancel = {
-        device = "/dev/nvme0n1";
-        type = "disk";
-        content = {
-          type = "gpt";
-          partitions = {
-            ESP = {
-              type = "EF00";
-              size = "512M";
-              content = {
-                type = "filesystem";
-                format = "vfat";
-                mountpoint = "/boot";
-                mountOptions = [ "umask=0077" ];
-              };
+  disko.devices.disk = {
+    grancel = {
+      device = "/dev/nvme0n1";
+      type = "disk";
+      content = {
+        type = "gpt";
+        partitions = {
+          ESP = {
+            type = "EF00";
+            size = "512M";
+            content = {
+              type = "filesystem";
+              format = "vfat";
+              mountpoint = "/boot";
+              mountOptions = [ "umask=0077" ];
             };
-            nix = {
-              size = "100%";
-              content = {
-                type = "filesystem";
-                format = "bcachefs";
-                mountpoint = "/";
-                mountOptions = [
-                  "defaults"
-                  "compression=lz4"
-                  "background=compression=lz4"
-                ];
-              };
+          };
+          nix = {
+            size = "100%";
+            content = {
+              type = "filesystem";
+              format = "bcachefs";
+              mountpoint = "/";
+              mountOptions = [
+                "defaults"
+                "compression=lz4"
+                "background=compression=lz4"
+              ];
             };
           };
         };
       };
-      gdata2 = {
-        device = "/dev/sda";
-        type = "disk";
-        content = {
-          type = "gpt";
-          partitions = {
-            data = {
-              size = "100%";
-              content = {
-                type = "btrfs";
-                mountpoint = "/media/gdata2";
-                mountOptions = [
-                  "nofail"
-                  "defaults"
-                  "compress=zstd"
-                ];
-              };
+    };
+    gdata2 = {
+      device = "/dev/sda";
+      type = "disk";
+      content = {
+        type = "gpt";
+        partitions = {
+          data = {
+            size = "100%";
+            content = {
+              type = "btrfs";
+              mountpoint = "/media/gdata2";
+              mountOptions = [
+                "nofail"
+                "defaults"
+                "compress=zstd"
+              ];
             };
           };
         };
--- hosts/jurai/disks.nix
+++ hosts/jurai/disks.nix
@@ -8,53 +8,51 @@
     persist = "/media/nixos/persist";
   };
 
-  disko.devices = {
-    disk.root = {
-      device = "/dev/vda";
-      type = "disk";
-      content = {
-        type = "gpt";
-        partitions = {
-          ESP = {
-            type = "EF00";
-            size = "512M";
-            content = {
-              type = "filesystem";
-              format = "vfat";
-              mountpoint = "/boot";
-              mountOptions = [ "umask=0077" ];
-            };
+  disko.devices.disk.root = {
+    device = "/dev/vda";
+    type = "disk";
+    content = {
+      type = "gpt";
+      partitions = {
+        ESP = {
+          type = "EF00";
+          size = "512M";
+          content = {
+            type = "filesystem";
+            format = "vfat";
+            mountpoint = "/boot";
+            mountOptions = [ "umask=0077" ];
           };
-          root = {
-            size = "100%";
-            content = {
-              type = "btrfs";
-              subvolumes = {
-                "/" = {
-                  mountpoint = "/media/nixos";
-                  mountOptions = [ "compress=zstd" ];
-                };
-                "/tmp_root" = {
-                  mountpoint = "/";
-                  mountOptions = [ "compress=zstd" ];
-                };
-                "/home" = {
-                  mountpoint = "/home";
-                  mountOptions = [ "compress=zstd" ];
-                };
-                "/nix" = {
-                  mountpoint = "/nix";
-                  mountOptions = [
-                    "noatime"
-                    "compress=zstd"
-                  ];
-                };
-                "/swap" = {
-                  mountpoint = "/var/lib/swap";
-                  swap.swapfile.size = "8G";
-                };
-                "/persist" = { };
+        };
+        root = {
+          size = "100%";
+          content = {
+            type = "btrfs";
+            subvolumes = {
+              "/" = {
+                mountpoint = "/media/nixos";
+                mountOptions = [ "compress=zstd" ];
               };
+              "/tmp_root" = {
+                mountpoint = "/";
+                mountOptions = [ "compress=zstd" ];
+              };
+              "/home" = {
+                mountpoint = "/home";
+                mountOptions = [ "compress=zstd" ];
+              };
+              "/nix" = {
+                mountpoint = "/nix";
+                mountOptions = [
+                  "noatime"
+                  "compress=zstd"
+                ];
+              };
+              "/swap" = {
+                mountpoint = "/var/lib/swap";
+                swap.swapfile.size = "8G";
+              };
+              "/persist" = { };
             };
           };
         };
