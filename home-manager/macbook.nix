ae59933b068d29f5c5a324e55197da6d7b858b94
Peter Rice <peterrice@fastmail.com>
2025-04-09 00:44:50 -0400

    clean up macbook config

--- home-manager/macbook.nix
+++ home-manager/macbook.nix
@@ -1,6 +1,9 @@
 { pkgs, ... }:
 {
-  imports = [ ./common.nix ];
+  imports = [
+    ./common.nix
+    ./firefox.nix
+  ];
 
   home = {
     username = "price";
@@ -9,23 +12,12 @@
 
   home.packages = with pkgs; [
     uutils-coreutils-noprefix
-    timg
     sarasa-gothic
     fantasque-sans-mono
   ];
 
-  # TODO 23.11: readd firefox module
-  # programs.firefox.package = null;
+  programs.firefox.package = null;
 
-  programs.fish = {
-    enable = true;
-    shellAbbrs = {
-      suod = "sudo";
-    };
-    shellAliases.rmi = "hub --git-dir=(realpath ~/dev/report-management-issues/.git) issue";
-  };
-
-  programs.bash.enable = false;
   programs.git.userEmail = "price@hubspot.com";
   programs.jujutsu.settings.user.email = "price@hubspot.com";
   programs.tmux.terminal = "screen-256color";
