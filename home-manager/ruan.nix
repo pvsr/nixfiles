63fe1185b9a2754496df2090bfdf188771da1ee0
Peter Rice <peterrice@fastmail.com>
2025-05-23 14:03:36 -0400

    ruan: enable firefox

--- home-manager/ruan.nix
+++ home-manager/ruan.nix
@@ -3,7 +3,10 @@
   flake.modules.homeManager.ruan =
     { pkgs, ... }:
     {
-      imports = [ self.modules.homeManager.nixos ];
+      imports = [
+        self.modules.homeManager.nixos
+        self.modules.homeManager.firefox
+      ];
 
       home.packages = with pkgs; [ nvtopPackages.amd ];
     };
