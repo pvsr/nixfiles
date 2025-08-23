a9d2bf23c0c07c07bbc20911747a66da78e0c162
Peter Rice <peterrice@fastmail.com>
2025-08-23 18:05:26 -0400

    bat: correct usage of batman

--- home-manager/bat.nix
+++ home-manager/bat.nix
@@ -3,11 +3,17 @@
   flake.modules.homeManager.core =
     { pkgs, ... }:
     {
-      home.packages = [ pkgs.bat-extras.batgrep ];
+      home.packages = with pkgs.bat-extras; [
+        batgrep
+        batman
+      ];
 
       programs.bat.enable = true;
       programs.bat.config.theme = "srcery";
       xdg.configFile."bat/themes/srcery.tmTheme".source = "${inputs.srcery-textmate}/srcery.tmTheme";
-      home.sessionVariables.MANPAGER = pkgs.bat-extras.batman;
+      home.sessionVariables.MANPAGER = pkgs.writeShellScript "batman" ''
+        awk '{ gsub(/\x1B\[[0-9;]*m/, "", $0); gsub(/.\x08/, "", $0); print }' \
+        | bat --plain --language man
+      '';
     };
 }
