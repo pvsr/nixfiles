0e89e11853dad7b40472bdc771bd8c1611413b12
Peter Rice <peterrice@fastmail.com>
2025-08-30 13:48:47 -0400

    i3: disable

--- modules/niri.nix
+++ modules/niri.nix
@@ -9,6 +9,9 @@
 
       programs.niri.enable = true;
       programs.niri.package = inputs.niri.packages.${pkgs.system}.niri-unstable;
+
+      environment.systemPackages = [ inputs.niri.packages.${pkgs.system}.xwayland-satellite-unstable ];
+
       systemd.user.services.niri-flake-polkit.serviceConfig.ExecStart =
         lib.mkForce "${pkgs.mate.mate-polkit}/libexec/polkit-mate-authentication-agent-1";
       services.gnome.gnome-keyring.enable = lib.mkForce false;
