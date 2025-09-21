b121df08e34606ebb5da7c61db8644bb89c038bb
Peter Rice <peterrice@fastmail.com>
2025-09-21 18:29:39 -0400

    agenix: encrypt secrets against dev machine key

--- hosts/ruan/secrets/_secrets.nix
+++ hosts/ruan/secrets/_secrets.nix
@@ -1,17 +1,18 @@
-let
-  ruan = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJi5+Pco0lVGVsAPRJl+FQd+c+AAsRHmZavWjaUgMsmO";
-  secrets = [
-    "radicale-users.age"
-    "transmission-credentials.json.age"
-    "miniflux-credentials.age"
-    "dns-token.age"
-  ];
-in
 builtins.listToAttrs (
-  map (name: {
-    inherit name;
-    value = {
-      publicKeys = [ ruan ];
-    };
-  }) secrets
+  map
+    (name: {
+      inherit name;
+      value.publicKeys = [
+        # host key
+        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJi5+Pco0lVGVsAPRJl+FQd+c+AAsRHmZavWjaUgMsmO"
+        # peter@grancel
+        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILACfyJt7+ULfX1XFhBbztlTMNDZnRNQbKj5DV2S7uVo"
+      ];
+    })
+    [
+      "radicale-users.age"
+      "transmission-credentials.json.age"
+      "miniflux-credentials.age"
+      "dns-token.age"
+    ]
 )
