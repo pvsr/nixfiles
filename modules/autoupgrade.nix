b5075f6f82a6bed1efdfbfcadd824e0595d864a3
Peter Rice <peterrice@fastmail.com>
2025-10-14 00:37:22 -0400

    disable autoupgrade

--- modules/autoupgrade.nix
+++ modules/autoupgrade.nix
@@ -1,6 +1,6 @@
 {
   flake.modules.nixos.core.system.autoUpgrade = {
-    enable = true;
+    enable = false;
     flake = "git+https://code.pvsr.dev/peter/nixos";
     dates = "06:00";
     persistent = false;
