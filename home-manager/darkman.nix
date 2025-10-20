321ae2d02fc61d0ff67b8895f72974b9bd5699f4
Peter Rice <peterrice@fastmail.com>
2025-10-20 01:06:47 -0400

    darkman: manage scripts with systemd

--- home-manager/darkman.nix
+++ home-manager/darkman.nix
@@ -1,26 +1,44 @@
+{ lib, ... }:
+let
+  modeToggleService = lib.mergeAttrs {
+    Install.WantedBy = [ "set-ui-mode.target" ];
+    Service.Type = "oneshot";
+    Service.RemainAfterExit = false;
+  };
+  activate = mode: ''
+    systemctl --user set-environment UI_MODE=${mode}
+    systemctl --user start set-ui-mode.target
+  '';
+in
 {
   flake.modules.homeManager.desktop =
     { pkgs, ... }:
-    let
-      scripts = mode: {
-        niri = ''
-          export NIRI_SOCKET=$(echo /run/user/$(id -u)/niri.*.sock)
-          niri msg action do-screen-transition
-        '';
-        gtk = ''
-          ${pkgs.dconf}/bin/dconf write /org/gnome/desktop/interface/color-scheme "'prefer-${mode}'"
+    {
+      systemd.user.targets.set-ui-mode.Unit = {
+        Description = "Switch to light/dark mode based on $UI_MODE";
+        StopWhenUnneeded = true;
+      };
+      systemd.user.services.set-gtk-scheme = modeToggleService {
+        Unit.Description = "Set GTK UI scheme";
+        Service.ExecStart = pkgs.writeShellScript "set-gtk-scheme" ''
+          ${pkgs.dconf}/bin/dconf write /org/gnome/desktop/interface/color-scheme "'prefer-$UI_MODE'"
         '';
       };
-    in
-    {
+      systemd.user.services.niri-screen-transition = modeToggleService {
+        Unit.Description = "Niri screen transition effect";
+        Service.ExecStart = pkgs.writeShellScript "niri-screen" ''
+          niri msg action do-screen-transition -d ''${UI_TRANSITION_MS:-250}
+        '';
+      };
+
       services.darkman = {
         enable = true;
         settings = {
           lat = 42.4;
           lng = -71.1;
         };
-        lightModeScripts = scripts "light";
-        darkModeScripts = scripts "dark";
+        darkModeScripts.target = activate "dark";
+        lightModeScripts.target = activate "light";
       };
     };
 }
