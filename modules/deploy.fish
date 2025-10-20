0903a27cddc46752e1cec7acd5a19bbed62c9242
Peter Rice <peterrice@fastmail.com>
2025-10-20 03:31:30 -0400

    deploy: bookmark changes, not commits

--- modules/deploy.fish
+++ modules/deploy.fish
@@ -1,5 +1,5 @@
 #!/usr/bin/env fish
-argparse -n deploy -X 1 'c/command=' 'f/flake=' r/remote -- $argv
+argparse -n deploy -X 1 c/command= r/remote -- $argv
 or return
 
 if test -e ./flake.nix
@@ -13,7 +13,7 @@ end
 test -d "$flake"
 and type -q jj
 and test (realpath "$flake") = "$(jj root 2>/dev/null)"
-and set commit (jj log -r 'heads(::@ ~ empty())' --no-graph -T commit_id)
+and set change (jj log -r 'heads(::@ ~ empty())' --no-graph -T change_id)
 
 if set -q argv[1]
     set host $argv[1]
@@ -30,5 +30,5 @@ set -a args (printf $_flag_command; or printf switch)
 nixos-rebuild $args --log-format internal-json --verbose &| nom --json
 
 test $pipestatus[1] = 0
-and set -q commit
-and jj --color=always bookmark set $host -B -r $commit 2&>/dev/null
+and set -q change
+and jj --color=always bookmark set $host -B -r $change 2&>/dev/null
