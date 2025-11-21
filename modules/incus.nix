d00eeaad3e6126ad9061cac7dc247d16e7dfee98
Peter Rice <peterrice@fastmail.com>
2025-11-21 14:44:06 -0500

    incus: use latest packaged version instead of lts

--- modules/incus.nix
+++ modules/incus.nix
@@ -1,18 +1,21 @@
 {
-  flake.modules.nixos.ruan = {
-    virtualisation.incus.enable = true;
+  flake.modules.nixos.ruan =
+    { pkgs, ... }:
+    {
+      virtualisation.incus.enable = true;
+      virtualisation.incus.package = pkgs.incus;
 
-    local.user.extraGroups = [ "incus-admin" ];
+      local.user.extraGroups = [ "incus-admin" ];
 
-    networking.firewall.interfaces.incusbr0.allowedTCPPorts = [
-      53
-      67
-    ];
-    networking.firewall.interfaces.incusbr0.allowedUDPPorts = [
-      53
-      67
-    ];
+      networking.firewall.interfaces.incusbr0.allowedTCPPorts = [
+        53
+        67
+      ];
+      networking.firewall.interfaces.incusbr0.allowedUDPPorts = [
+        53
+        67
+      ];
 
-    environment.persistence.nixos.directories = [ "/var/lib/incus" ];
-  };
+      environment.persistence.nixos.directories = [ "/var/lib/incus" ];
+    };
 }
