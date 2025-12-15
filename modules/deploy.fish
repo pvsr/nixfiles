50afc65b450022c71c3fe333886d97e36490549b
Peter Rice <peterrice@fastmail.com>
2025-12-15 14:21:29 -0500

    deploy: add gcroots and jj revision flag

--- modules/deploy.fish
+++ modules/deploy.fish
@@ -1,5 +1,5 @@
 #!/usr/bin/env fish
-argparse -n deploy -X 1 c/command= r/remote -- $argv
+argparse -n deploy -X 1 c/command= r/revision= R/remote -- $argv
 or return
 
 if test -e ./flake.nix
@@ -10,10 +10,11 @@ else
     set flake git+ssh://grancel:/etc/nixos/
 end
 
-test -d "$flake"
-and type -q jj
-and test (realpath "$flake") = "$(jj root 2>/dev/null)"
-and set change (jj log -r 'heads(::@ ~ empty())' --no-graph -T change_id)
+if test -d $flake; and type -q jj; and test (realpath $flake) = "$(jj root 2>/dev/null)"
+    set revision (printf $_flag_revision; or printf 'heads(::@ ~ empty())')
+    set commit (jj log -r $revision --no-graph -T commit_id)
+    set flake "$flake?rev=$commit&ref=$commit"
+end
 
 if set -q argv[1]
     set host $argv[1]
@@ -27,8 +28,15 @@ end
 set -a args --no-reexec --sudo --flake $flake#$host
 set -a args (printf $_flag_command; or printf switch)
 
-nixos-rebuild $args --log-format internal-json --verbose &| nom --json
+if not set -q _flag_remote; and test -d ~/.local/share/nix/gcroots
+    set result (nix build $flake#nixosConfigurations.$host.config.system.build.toplevel \
+        --print-out-paths --log-format internal-json --verbose 2>| nom --json)
+    test $pipestatus[1] = 0; or return
+    nix-store --realise $result --add-root ~/.local/share/nix/gcroots/$host
+end
 
-test $pipestatus[1] = 0
-and set -q change
-and jj --color=always bookmark set $host -B -r $change 2&>/dev/null
+nixos-rebuild $args --log-format internal-json --verbose 2>| nom --json
+test $pipestatus[1] = 0; or return
+
+set -q commit; or return
+jj --color=always bookmark set $host -B -r $commit 2&>/dev/null
