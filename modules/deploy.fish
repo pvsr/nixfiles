2f8e75b0abbac5d178b5707eccfc355cb9ba5120
Peter Rice <peterrice@fastmail.com>
2026-01-13 00:54:42 -0500

    deploy script: refactor and add completions

--- modules/deploy.fish
+++ modules/deploy.fish
@@ -1,4 +1,3 @@
-#!/usr/bin/env fish
 argparse -n deploy -X 1 c/command= r/revision= R/remote -- $argv
 or return
 
@@ -7,13 +6,18 @@ if test -e ./flake.nix
 else if test -e /etc/nixos/flake.nix
     set flake /etc/nixos
 else
-    set flake git+ssh://grancel:/etc/nixos/
+    set flake git+ssh://grancel.ygg.pvsr.dev:/etc/nixos/
 end
 
-if test -d $flake; and type -q jj; and test (realpath $flake) = "$(jj root 2>/dev/null)"
+test -d $flake; and type -q jj
+and test (realpath $flake) = "$(jj root 2>/dev/null)"
+and begin
     set revision (printf $_flag_revision; or printf 'heads(::@ ~ empty())')
     set commit (jj log -r $revision --no-graph -T commit_id)
-    set flake "$flake?rev=$commit&ref=$commit"
+    or return
+
+    type -q git; and git rev-parse &>/dev/null
+    and set flake "$flake?rev=$commit&ref=$commit"
 end
 
 if set -q argv[1]
@@ -25,18 +29,24 @@ if set -q argv[1]
 else
     set host (hostname)
 end
+
 set -a args --no-reexec --sudo --flake $flake#$host
+
 set -a args (printf $_flag_command; or printf switch)
 
 if not set -q _flag_remote; and test -d ~/.local/share/nix/gcroots
-    set result (nix build $flake#nixosConfigurations.\"$host\".config.system.build.toplevel \
+    set result (nix build \
+        $flake#nixosConfigurations.\"$host\".config.system.build.toplevel \
         --print-out-paths --log-format internal-json --verbose 2>| nom --json)
     test $pipestatus[1] = 0; or return
-    nix-store --realise $result --add-root ~/.local/share/nix/gcroots/$host
 end
 
 nixos-rebuild $args --log-format internal-json --verbose 2>| nom --json
 test $pipestatus[1] = 0; or return
 
-set -q commit; or return
-jj --color=always bookmark set $host -B -r $commit 2&>/dev/null
+set -q commit
+and jj --color=always bookmark set $host -B -r $commit &>/dev/null
+
+set -q result
+and nix-store --realise $result \
+    --add-root ~/.local/share/nix/gcroots/$host &>/dev/null
