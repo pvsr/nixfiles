1516c6cd3a96a1fa564425661cb1e85b24694f23
Peter Rice <peterrice@fastmail.com>
2026-01-20 02:03:15 -0500

    deploy script: take multiple host args

--- modules/deploy.fish
+++ modules/deploy.fish
@@ -1,4 +1,4 @@
-argparse -n deploy -X 1 c/command= r/revision= R/remote -- $argv
+argparse -n deploy c/command= r/revision= R/remote -- $argv
 or return
 
 if test -e ./flake.nix
@@ -20,34 +20,49 @@ and begin
     and set flake "$flake?rev=$commit&ref=$commit"
 end
 
-if set -q argv[1]
-    set host $argv[1]
+function icon -a seed
+    random (echo -n $seed | sum | cut -d' ' -f1)
+    random choice 🌸 🌼 🌻 🌺 🌷 🍄 🍀 🌳
+end
+
+function deploy -a host
     set host_url (string replace incus (hostname) $host).ygg.pvsr.dev
     set args --target-host $host_url
     if set -q _flag_remote
         set -a args --build-host $host_url
     end
-else
-    set host (hostname)
-end
 
-set -a args --no-reexec --sudo --flake $flake#$host
+    set -a args --no-reexec --sudo --flake $flake#$host
 
-set -a args (printf $_flag_command; or printf switch)
+    set -a args (printf $_flag_command; or printf switch)
 
-if not set -q _flag_remote; and test -d ~/.local/share/nix/gcroots
-    set result (nix build \
-        $flake#nixosConfigurations.\"$host\".config.system.build.toplevel \
-        --print-out-paths --log-format internal-json --verbose 2>| nom --json)
+    if not set -q _flag_remote; and test -d ~/.local/share/nix/gcroots
+        set result (nix build \
+            $flake#nixosConfigurations.\"$host\".config.system.build.toplevel \
+            --print-out-paths --log-format internal-json --verbose 2>| nom --json)
+        test $pipestatus[1] = 0; or return
+    end
+
+    nixos-rebuild $args --log-format internal-json --verbose 2>| nom --json
     test $pipestatus[1] = 0; or return
+
+    set -q commit
+    and jj --color=always bookmark set $host -B -r $commit &>/dev/null
+
+    set -q result
+    and nix-store --realise $result \
+        --add-root ~/.local/share/nix/gcroots/$host &>/dev/null
+
+    echo
+    set highlight (set_color -o brmagenta)
+    set reset (set_color normal)
+    set icon (icon $host)
+    echo Deployed $icon$highlight$host_url$reset$icon
 end
 
-nixos-rebuild $args --log-format internal-json --verbose 2>| nom --json
-test $pipestatus[1] = 0; or return
-
-set -q commit
-and jj --color=always bookmark set $host -B -r $commit &>/dev/null
-
-set -q result
-and nix-store --realise $result \
-    --add-root ~/.local/share/nix/gcroots/$host &>/dev/null
+if not set -q argv[1]
+    set argv (hostname)
+end
+for arg in $argv
+    deploy $arg
+end
