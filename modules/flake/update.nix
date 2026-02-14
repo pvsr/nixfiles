ca135653755d1da9f57f48c3b1ed175d955e3f29
Peter Rice <peterrice@fastmail.com>
2026-02-14 14:51:32 -0500

    flake-update: use config.nix.package

--- modules/flake/update.nix
+++ modules/flake/update.nix
@@ -1,51 +1,53 @@
 {
-  flake.modules.hjem.grancel =
-    { pkgs, ... }:
+  local.desktops.grancel =
+    { config, pkgs, ... }:
     {
-      systemd = {
-        services.update-flake.path = with pkgs; [
-          lix
-          jujutsu
-          git
-          openssh
-        ];
-        services.update-flake.serviceConfig = {
-          Type = "oneshot";
-          ExecStart = pkgs.writers.writeFish "update-flake" ''
-            test -d /etc/nixos; or exit 0
-            cd /etc/nixos
-            jj root; or exit 0
+      hjem.extraModules = [
+        {
+          systemd.services.update-flake.path = [
+            config.nix.package
+            pkgs.jujutsu
+            pkgs.git
+            pkgs.openssh
+          ];
+          systemd.services.update-flake.serviceConfig = {
+            Type = "oneshot";
+            ExecStart = pkgs.writers.writeFish "update-flake" ''
+              test -d /etc/nixos; or exit 0
+              cd /etc/nixos
+              jj root; or exit 0
 
-            jj new --quiet
-            set restore (jj log --no-graph -r @- -T 'change_id.shortest(8)')
-            echo Will restore $restore
+              jj new --quiet
+              set restore (jj log --no-graph -r @- -T 'change_id.shortest(8)')
+              echo Will restore $restore
 
-            set remote github
-            echo Fetching remote $remote
-            jj git fetch --remote $remote
-            jj new main@$remote
+              set remote github
+              echo Fetching remote $remote
+              jj git fetch --remote $remote
+              jj new main@$remote
 
-            echo Starting flake update
-            git config set --local user.name 'Peter Rice (automated)'
-            git config set --local user.email noreply@pvsr.dev
-            nix flake update --commit-lock-file
-            git config unset --local user.name
-            git config unset --local user.email
+              echo Starting flake update
+              git config set --local user.name 'Peter Rice (automated)'
+              git config set --local user.email noreply@pvsr.dev
+              nix flake update --commit-lock-file
+              git config unset --local user.name
+              git config unset --local user.email
 
-            set bookmark flake-update
-            jj bookmark set -B -r "@- ~ main@$remote" $bookmark
-            and jj git push --remote $remote --bookmark $bookmark
+              set bookmark flake-update
+              jj bookmark set -B -r "@- ~ main@$remote" $bookmark
+              and jj git push --remote $remote --bookmark $bookmark
 
-            jj edit $restore
-          '';
-        };
-        timers.update-flake = {
-          timerConfig = {
-            OnCalendar = "Fri *-*-* 06:00";
-            Persistent = true;
+              jj edit $restore
+            '';
           };
-          wantedBy = [ "timers.target" ];
-        };
-      };
+          systemd.timers.update-flake = {
+            timerConfig = {
+              OnCalendar = "Fri *-*-* 07:00";
+              Persistent = true;
+            };
+            wantedBy = [ "timers.target" ];
+          };
+        }
+      ];
     };
 }
