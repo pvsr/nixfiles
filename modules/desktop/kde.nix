5546ae15ed88a8aefe046fbb33924713fb19142d
Peter Rice <peterrice@fastmail.com>
2026-02-14 01:38:58 -0500

    niri: allow disabling module

--- modules/desktop/kde.nix
+++ modules/desktop/kde.nix
@@ -3,6 +3,7 @@
   flake.modules.nixos.kde =
     { config, pkgs, ... }:
     {
+      local.niri.enable = false;
       services.desktopManager.plasma6.enable = !config.boot.isContainer;
       services.speechd.enable = lib.mkForce false;
       environment.plasma6.excludePackages = with pkgs.kdePackages; [
--- modules/desktop/niri/default.nix
+++ modules/desktop/niri/default.nix
@@ -10,16 +10,21 @@
         inputs.niri.nixosModules.niri
       ];
 
-      programs.niri.enable = true;
-      programs.niri.package = inputs.niri.packages.${system}.niri-unstable;
+      options.local.niri.enable = lib.mkEnableOption { default = true; };
 
-      environment.systemPackages = [
-        inputs.niri.packages.${system}.xwayland-satellite-unstable
-      ];
+      config = lib.mkIf config.local.niri.enable {
 
-      systemd.user.services.niri-flake-polkit.serviceConfig.ExecStart =
-        lib.mkForce "${pkgs.mate.mate-polkit}/libexec/polkit-mate-authentication-agent-1";
-      services.gnome.gnome-keyring.enable = lib.mkForce false;
+        programs.niri.enable = true;
+        programs.niri.package = inputs.niri.packages.${system}.niri-unstable;
+
+        environment.systemPackages = [
+          inputs.niri.packages.${system}.xwayland-satellite-unstable
+        ];
+
+        systemd.user.services.niri-flake-polkit.serviceConfig.ExecStart =
+          lib.mkForce "${pkgs.mate.mate-polkit}/libexec/polkit-mate-authentication-agent-1";
+        services.gnome.gnome-keyring.enable = lib.mkForce false;
+      };
     };
 
   flake.modules.hjem.core.options.niri.extraConfig = lib.mkOption {
