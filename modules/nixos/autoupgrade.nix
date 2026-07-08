76d93b632dd567ea42ea211d7ddb828cf85fd63e
Peter Rice <peterrice@fastmail.com>
2026-07-07 20:21:47 -0400

    run forgejo in a microvm

--- modules/nixos/autoupgrade.nix
+++ modules/nixos/autoupgrade.nix
@@ -1,7 +1,7 @@
 {
   flake.modules.nixos.core.system.autoUpgrade = {
     enable = true;
-    flake = "git+https://code.pvsr.dev/peter/nixos";
+    flake = "git+https://forgejo.grancel.ygg.pvsr.dev/peter/nixos";
     dates = "06:00";
     persistent = false;
   };
--- modules/services/forgejo.nix
+++ modules/services/forgejo.nix
@@ -1,84 +1,97 @@
 { self, ... }:
 let
-  hosts = self.nixosConfigurations;
-  domains.internal = "code.${hosts.ruan.config.networking.fqdn}";
-  domains.external = "code.pvsr.dev";
+  domain = "code.pvsr.dev";
+  settings = {
+    server = {
+      PROTOCOL = "http";
+      HTTP_ADDR = "::";
+      HTTP_PORT = 80;
+      DOMAIN = domain;
+      ROOT_URL = "https://${domain}";
+      START_SSH_SERVER = true;
+      SSH_LISTEN_HOST = "::";
+      BUILTIN_SSH_SERVER_USER = "git";
+    };
+    DEFAULT.APP_NAME = domain;
+    "ui.meta" = {
+      AUTHOR = domain;
+      DESCRIPTION = domain;
+    };
+    i18n = {
+      LANGS = "en-US";
+      NAMES = "English";
+    };
+    other = {
+      SHOW_FOOTER_VERSION = false;
+      SHOW_FOOTER_TEMPLATE_LOAD_TIME = false;
+      SHOW_FOOTER_POWERED_BY = false;
+    };
+    repository = {
+      ENABLE_PUSH_CREATE_USER = true;
+      DEFAULT_PUSH_CREATE_PRIVATE = false;
+      DISABLED_REPO_UNITS = builtins.concatStringsSep "," [
+        "repo.issues"
+        "repo.ext_issues"
+        "repo.pulls"
+        "repo.wiki"
+        "repo.ext_wiki"
+        "repo.projects"
+        "repo.packages"
+        "repo.actions"
+      ];
+      DISABLE_STARS = true;
+      DISABLE_DOWNLOAD_SOURCE_ARCHIVES = true;
+    };
+    "repository.upload".ENABLED = false;
+    service.DISABLE_REGISTRATION = true;
+    openid.ENABLE_OPENID_SIGNIN = false;
+    oauth2.ENABLED = false;
+    security = {
+      INSTALL_LOCK = true;
+      LOGIN_REMEMBER_DAYS = 365;
+    };
+    api.ENABLE_SWAGGER = false;
+    cache = {
+      ADAPTER = "twoqueue";
+      HOST = ''{"size":100, "recent_ratio":0.25, "ghost_ratio":0.5}'';
+    };
+  };
 in
 {
-  local.servers.crossbell.local.caddy.reverseProxies.${domains.external} = domains.internal;
-
-  local.containers."code.ruan" =
-    { config, pkgs, ... }:
+  flake.modules.nixos.forgejo =
+    { pkgs, config, ... }:
     {
-      environment.systemPackages = [ pkgs.forgejo ];
-      environment.sessionVariables.FORGEJO_WORK_DIR = "/var/lib/forgejo";
+      environment.persistence.nixos.directories = [ "/var/lib/forgejo" ];
 
-      networking.firewall.allowedTCPPorts = [
-        80
-        2222
-      ];
+      vms.forgejo = {
+        environment.systemPackages = [ pkgs.forgejo ];
+        environment.sessionVariables.FORGEJO_WORK_DIR = "/var/lib/forgejo";
+        microvm.shares = [
+          {
+            source = "/var/lib/forgejo";
+            mountPoint = "/var/lib/forgejo";
+            tag = "forgejo";
+            proto = "virtiofs";
+          }
+        ];
 
-      local.testScript = ''
-        machine.wait_for_unit("forgejo.service")
-        machine.wait_for_open_port(80)
-        machine.wait_for_open_port(2222)
-      '';
+        boot.kernel.sysctl."net.ipv4.ip_unprivileged_port_start" = 22;
+        networking.firewall.allowedTCPPorts = [
+          22
+          80
+        ];
+        services.openssh.ports = [ 2222 ];
 
-      services.forgejo = {
-        enable = true;
-        package = pkgs.forgejo;
-        settings = {
-          server = {
-            PROTOCOL = "http";
-            HTTP_ADDR = "::";
-            HTTP_PORT = 80;
-            DOMAIN = "${domains.external}";
-            ROOT_URL = "https://${domains.external}";
-            START_SSH_SERVER = true;
-            SSH_DOMAIN = "${domains.internal}";
-            SSH_PORT = 2222;
-            SSH_LISTEN_HOST = "::";
-            SSH_LISTEN_PORT = 2222;
-            BUILTIN_SSH_SERVER_USER = "git";
-          };
-          DEFAULT.APP_NAME = "${domains.external}";
-          "ui.meta" = {
-            AUTHOR = "${domains.external}";
-            DESCRIPTION = "${domains.external}";
-          };
-          i18n = {
-            LANGS = "en-US";
-            NAMES = "English";
-          };
-          other = {
-            SHOW_FOOTER_VERSION = false;
-            SHOW_FOOTER_TEMPLATE_LOAD_TIME = false;
-            SHOW_FOOTER_POWERED_BY = false;
-          };
-          repository = {
-            PREFERRED_LICENSES = "MIT,GPL-3.0-or-later,AGPL-3.0-or-later";
-            ENABLE_PUSH_CREATE_USER = true;
-            DEFAULT_PUSH_CREATE_PRIVATE = false;
-            DISABLED_REPO_UNITS = "repo.issues,repo.ext_issues,repo.pulls,repo.wiki,repo.ext_wiki,repo.projects,repo.packages,repo.actions";
-            DISABLE_STARS = true;
-            DISABLE_DOWNLOAD_SOURCE_ARCHIVES = true;
-          };
-          "repository.upload".ENABLED = false;
-          service = {
-            DISABLE_REGISTRATION = true;
-          };
-          openid.ENABLE_OPENID_SIGNIN = false;
-          oauth2.ENABLED = false;
-          security = {
-            INSTALL_LOCK = true;
-            LOGIN_REMEMBER_DAYS = 365;
-          };
-          api.ENABLE_SWAGGER = false;
-          cache = {
-            ADAPTER = "twoqueue";
-            HOST = ''{"size":100, "recent_ratio":0.25, "ghost_ratio":0.5}'';
-          };
+        services.forgejo = {
+          enable = true;
+          package = pkgs.forgejo;
+          inherit settings;
         };
       };
     };
+
+  local.desktops.grancel.imports = [ self.modules.nixos.forgejo ];
+
+  local.servers.crossbell.local.caddy.reverseProxies.${domain} =
+    self.nixosConfigurations.grancel.config.microvm.vms.forgejo.config.config.networking.fqdn;
 }
