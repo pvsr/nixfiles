cd9d672d435b59ae0f1de811d593cedffdcda528
Peter Rice <peterrice@fastmail.com>
2026-08-22 11:00:48 -0400

    build flake update branch locally

--- modules/nixos/autoupgrade.nix
+++ modules/nixos/autoupgrade.nix
@@ -1,8 +1,37 @@
+let
+  flake = "github:pvsr/nixfiles";
+in
 {
   flake.modules.nixos.core.system.autoUpgrade = {
     enable = true;
-    flake = "git+https://forgejo.grancel.ygg.pvsr.dev/peter/nixos";
-    dates = "06:00";
+    inherit flake;
+    dates = "07:00";
     persistent = false;
+  };
+
+  local.desktops.grancel = { config, pkgs, ... }: {
+    hjem.extraModules = [
+      {
+        systemd.services.rebuild-os = {
+          path = [
+            config.nix.package
+            pkgs.nh
+          ];
+          serviceConfig.Type = "oneshot";
+          serviceConfig.ExecStart = pkgs.writers.writeFish "rebuild-os" ''
+            for branch in main update_flake_lock_action
+              nh os build ${flake}/$branch
+            end
+          '';
+        };
+        systemd.timers.rebuild-os = {
+          timerConfig = {
+            OnCalendar = "Wed,Fri *-*-* 06:00";
+            Persistent = true;
+          };
+          wantedBy = [ "timers.target" ];
+        };
+      }
+    ];
   };
 }
